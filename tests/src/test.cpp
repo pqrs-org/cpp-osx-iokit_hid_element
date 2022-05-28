e30d864b0bf03bc35a122413475551c6cfe23b90
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-28 21:24:47 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,36 +1,41 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/osx/iokit_hid_element.hpp>
 
-TEST_CASE("iokit_hid_element nullptr") {
-  pqrs::osx::iokit_hid_element e;
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-  REQUIRE(e.get_cf_ptr() == false);
-  REQUIRE(e.get_raw_ptr() == nullptr);
-  REQUIRE(e.get_logical_max() == std::nullopt);
-  REQUIRE(e.get_logical_min() == std::nullopt);
-  REQUIRE(e.get_name() == std::nullopt);
-  REQUIRE(e.get_physical_max() == std::nullopt);
-  REQUIRE(e.get_physical_min() == std::nullopt);
-  REQUIRE(e.get_report_count() == std::nullopt);
-  REQUIRE(e.get_report_id() == std::nullopt);
-  REQUIRE(e.get_report_size() == std::nullopt);
-  REQUIRE(e.get_type() == std::nullopt);
-  REQUIRE(e.get_unit() == std::nullopt);
-  REQUIRE(e.get_unit_exponent() == std::nullopt);
-  REQUIRE(e.get_usage_page() == std::nullopt);
-  REQUIRE(e.get_usage() == std::nullopt);
-  REQUIRE(e.has_null_state() == std::nullopt);
-  REQUIRE(e.has_preferred_state() == std::nullopt);
-  REQUIRE(e.is_array() == std::nullopt);
-  REQUIRE(e.is_non_linear() == std::nullopt);
-  REQUIRE(e.is_relative() == std::nullopt);
-  REQUIRE(e.is_virtual() == std::nullopt);
-  REQUIRE(e.is_wrapping() == std::nullopt);
-  REQUIRE(e == false);
-}
+  "iokit_hid_element nullptr"_test = [] {
+    pqrs::osx::iokit_hid_element e;
 
-TEST_CASE("get_iokit_hid_element_type_name") {
-  REQUIRE(pqrs::osx::get_iokit_hid_element_type_name(pqrs::osx::iokit_hid_element_type::input_misc) == std::string("input_misc"));
+    expect(e.get_cf_ptr() == false);
+    expect(e.get_raw_ptr() == nullptr);
+    expect(e.get_logical_max() == std::nullopt);
+    expect(e.get_logical_min() == std::nullopt);
+    expect(e.get_name() == std::nullopt);
+    expect(e.get_physical_max() == std::nullopt);
+    expect(e.get_physical_min() == std::nullopt);
+    expect(e.get_report_count() == std::nullopt);
+    expect(e.get_report_id() == std::nullopt);
+    expect(e.get_report_size() == std::nullopt);
+    expect(e.get_type() == std::nullopt);
+    expect(e.get_unit() == std::nullopt);
+    expect(e.get_unit_exponent() == std::nullopt);
+    expect(e.get_usage_page() == std::nullopt);
+    expect(e.get_usage() == std::nullopt);
+    expect(e.has_null_state() == std::nullopt);
+    expect(e.has_preferred_state() == std::nullopt);
+    expect(e.is_array() == std::nullopt);
+    expect(e.is_non_linear() == std::nullopt);
+    expect(e.is_relative() == std::nullopt);
+    expect(e.is_virtual() == std::nullopt);
+    expect(e.is_wrapping() == std::nullopt);
+    expect(e == false);
+  };
+
+  "get_iokit_hid_element_type_name"_test = [] {
+    expect(pqrs::osx::get_iokit_hid_element_type_name(pqrs::osx::iokit_hid_element_type::input_misc) == std::string("input_misc"));
+  };
+
+  return 0;
 }
